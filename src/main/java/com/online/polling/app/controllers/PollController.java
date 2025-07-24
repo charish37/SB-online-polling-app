4da0853acba082693ff20b192121ddfe45959700
nadipaca <nadipaca@mail.uc.edu>
2025-07-24 19:37:10 -0400

    Add basic polling services and controllers

---
+++ src/main/java/com/online/polling/app/controllers/PollController.java
@@ -0,0 +1,26 @@
+package com.online.polling.app.controllers;
+
+import com.online.polling.app.model.Poll;
+import com.online.polling.app.services.PollService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/polls")
+public class PollController {
+
+    @Autowired
+    private PollService pollService;
+
+    @PostMapping
+    public Poll createPoll(@RequestBody Poll poll){
+        return pollService.createPoll(poll);
+    }
+
+    @GetMapping
+    public List<Poll> getAllPolls(){
+        return pollService.getAllPolls();
+    }
+}
