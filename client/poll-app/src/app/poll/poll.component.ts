b40ddd12a0657523625e2e60a2ad19ed28745ed1
nadipaca <nadipaca@mail.uc.edu>
2025-07-25 18:34:34 -0400

    implement voting functionality and reset poll feature; update HTML and TypeScript components

--- client/poll-app/src/app/poll/poll.component.ts
+++ client/poll-app/src/app/poll/poll.component.ts
@@ -50,6 +50,29 @@ export class PollComponent implements OnInit {
   })
  }
 
+ resetPoll(){
+   this.newPoll = {
+    id: 0,
+    question: ' ',
+    options: [
+      {voteOption: '', voteCount: 0},
+      {voteOption: '', voteCount: 0},
+    ]
+  }
+ }
+
+ vote(pollId: number, optionIndex: number){
+   this.pollService.vote(pollId, optionIndex).subscribe({
+    next:() => {
+      const poll = this.polls.find(p => p.id == pollId);
+      if(poll){
+        poll.options[optionIndex].voteCount++;
+      }
+    },
+    error: (error) => {console.error("error voting on a poll")}
+   });
+ }
+
  trackByIndex(index: number): number {
   return index;
  }
