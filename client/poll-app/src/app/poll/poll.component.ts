f03aee8db3db5077692ebbe762eeff63911ccd13
nadipaca <nadipaca@mail.uc.edu>
2025-07-25 18:11:28 -0400

    add ui for poll

--- client/poll-app/src/app/poll/poll.component.ts
+++ client/poll-app/src/app/poll/poll.component.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PollService } from '../poll.service';
+import { Poll } from '../poll.models';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-poll',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './poll.component.html',
   styleUrl: './poll.component.css'
 })
-export class PollComponent {
+export class PollComponent implements OnInit {
+  newPoll: Poll = {
+    id: 0,
+    question: ' ',
+    options: [
+      {voteOption: '', voteCount: 0},
+      {voteOption: '', voteCount: 0},
+    ]
+  }
+  polls: Poll[] = [];
 
+ constructor(private pollService: PollService){}
+
+ ngOnInit(): void {
+     this.loadPolls(); //loads the data on comp initialised
+ }
+
+ loadPolls() { //to fetch all polls from backend
+   this.pollService.getPolls().subscribe({
+      next: (data) => {
+        console.log("polls received", data);
+        this.polls = data;
+      },
+      error: (error) => {
+        console.error("Error fetching polls: ", error);
+      }
+   });
+ }
+
+ createPoll(){
+  this.pollService.createPoll(this.newPoll).subscribe({
+    next: (createdPoll) => {
+      this.polls.push(createdPoll);
+    },
+    error: (error) => {
+      console.error("Error creating poll");
+    }
+  })
+ }
+
+ trackByIndex(index: number): number {
+  return index;
+ }
 }
