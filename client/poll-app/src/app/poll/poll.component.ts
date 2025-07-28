a3d6cfea25a0ac0583b3f92cd7756934971e7eba
nadipaca <nadipaca@mail.uc.edu>
2025-07-28 19:42:31 -0400

    add learning notes and enhance poll component with option removal and dynamic addition

--- client/poll-app/src/app/poll/poll.component.ts
+++ client/poll-app/src/app/poll/poll.component.ts
@@ -27,6 +27,11 @@ export class PollComponent implements OnInit {
      this.loadPolls(); //loads the data on comp initialised
  }
 
+ //subscribe in the poll component file is a method used to handle RxJS observables - which are used for asynchronous operations in angular.
+//  subscribe is a method allows to listen to data streams from HTTP requests.
+// Handles the response when it successfully arrives using next
+//  Manage errors if something goes wrong using error.
+
  loadPolls() { //to fetch all polls from backend
    this.pollService.getPolls().subscribe({
       next: (data) => {
@@ -37,6 +42,10 @@ export class PollComponent implements OnInit {
         console.error("Error fetching polls: ", error);
       }
    });
+ }
+
+ addOption(){
+  this.newPoll.options.push({voteOption: '', voteCount: 0});
  }
 
  createPoll(){
@@ -73,7 +82,32 @@ export class PollComponent implements OnInit {
    });
  }
 
+//  Angular uses the trackBy function to determine which items in a list have changed, deleted, been added or removed when the list is updated.
  trackByIndex(index: number): number {
   return index;
  }
+
+
+// 1.  Without tracking angular tracks by object reference.
+// <div *ngFor="let option of newPoll.options">
+//  In the above angular compare object by reference. So, if the array changes, angular recreates all DOM elements.
+// This cause performance issue -- expensive for larger lists.
+
+// 2. With trackBy: trackByIndex
+//  <div *ngFor="let option of newPoll.options; let i = index; trackBy: trackByIndex ">
+// Angular uses index to track items
+// Only DOM elements at changed positions are updated
+// Performance is better
+
+// trackByIndex(index: number, item: any): number{
+//   return item.id || indexedDB;
+// }
+
+// Before: ['Option 1', 'Option 2', 'Option 3']
+// After:  ['Option 1', 'Option 3']
+
+// With trackByIndex:
+// - Index 0 (Option 1): No change - DOM element reused
+// - Index 1 (Option 3): Content changed - DOM element updated
+// - Index 2: Removed - DOM element destroyed
 }
